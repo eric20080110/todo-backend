d683187fa8383d21056782ce120008a5c14ed6f1
air904 <air904@air904deMacBook-Pro-2.local>
2026-04-10 11:33:24 +0800

    第一次上傳後端

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const mongoose = require('mongoose');
+
+// 啟動中間件
+app.use(cors()); // 允許你的 React (localhost:5173) 跟後端 (localhost:3000) 溝通
+app.use(express.json()); // 讓後端看得懂前端傳來的 JSON 資料
+
+const dbURI = 'mongodb+srv://eric20080110_db_user:eric970110@tododata.67qp4fs.mongodb.net/?appName=tododata';
+
+mongoose.connect(dbURI)
+  .then(() => console.log('✅ 成功連線到 MongoDB 資料庫！'))
+  .catch((err) => console.log('❌ 資料庫連線失敗：', err));
+
+
+const todoSchema = new mongoose.Schema({
+  content: { type: String, required: true }, // 必填文字
+  checked: { type: Boolean, default: false },// 預設為 false
+  detailcontent: { type: String, default: '' },
+  remindertime: { type: String, default: '' }
+});
+
+const Todo = mongoose.model('Todo', todoSchema);
+// 1. 讀取清單 (GET)
+app.get('/api/todos', async (req, res) => {
+  // Todo.find() 會去資料庫把所有資料抓出來
+  const allTodos = await Todo.find(); 
+  res.json(allTodos);
+});
+
+// 2. 新增事項 (POST)
+app.post('/api/todos', async (req, res) => {
+  // 用前端傳來的資料，創建一筆新記錄
+  const newTodo = await Todo.create({
+    content: req.body.content
+  });
+  res.json(newTodo);
+});
+
+// 3. 刪除事項 (DELETE)
+app.delete('/api/todos/:id', async (req, res) => {
+  // Mongoose 內建的神奇方法：靠 ID 尋找並刪除
+  await Todo.findByIdAndDelete(req.params.id);
+  res.json({ message: '刪除成功' });
+});
+
+// 4. 更新狀態或細節 (PUT)
+app.put('/api/todos/:id', async (req, res) => {
+  // findByIdAndUpdate(要找的ID, 要更新的資料, { new: true 回傳更新後的最新結果 })
+  const updatedTodo = await Todo.findByIdAndUpdate(
+    req.params.id, 
+    { ...req.body }, 
+    { new: true }
+  );
+  
+  if (updatedTodo) {
+    res.json({ message: '更新成功', data: updatedTodo });
+  } else {
+    res.status(404).json({ message: '找不到此項目' });
+  }
+});
+
+const PORT = process.env.PORT || 3000; 
+
+app.listen(PORT, () => {
+  console.log(`後端伺服器已啟動，監聽 Port: ${PORT}`);
+});
